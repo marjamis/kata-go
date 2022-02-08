82ec40f21a652ae491ad05c37a79c1dec26444a4
marjamis <10202014+marjamis@users.noreply.github.com>
2022-02-08 11:54:08 +1100

    feat(gorilla/mux): Added sample handlers
    These are example handlers with some tests for reference.
    Also allowing for expansion as required in the future.

--- gorilla-mux/internal/pkg/serve/engine.go
+++ gorilla-mux/internal/pkg/serve/engine.go
@@ -1,8 +1,56 @@
 package serve
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 // Serve is the entry of running the webserver that will use gorilla/mux
 func Serve() {
-	fmt.Println("Stub")
+	r := createMuxRouter()
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:" + os.Getenv("PORT"),
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func createMuxRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/products/{key}", ProductHandler)
+	r.HandleFunc("/articles/{category}", ArticlesCategoryHandler)
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+
+	return r
+}
+
+// ProductHandler is used for the route that gets information about a product
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Product: %v\n", vars["key"])
+}
+
+// ArticlesCategoryHandler is used for the route that gets information about an articles category
+func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
+
+// ArticleHandler is used for the route that gets an article from a specific category
+func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v - Article: %v\n", vars["category"], vars["id"])
 }
--- gorilla-mux/internal/pkg/serve/engine_test.go
+++ gorilla-mux/internal/pkg/serve/engine_test.go
@@ -1,1 +1,96 @@
 package serve
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func requestGeneration(method string, path string, body io.Reader) (*httptest.ResponseRecorder, error) {
+	router := createMuxRouter()
+
+	req, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	rr := httptest.NewRecorder()
+
+	router.ServeHTTP(rr, req)
+
+	return rr, nil
+}
+func TestProductHandler(t *testing.T) {
+	t.Run("Valid product key provided", func(t *testing.T) {
+		rr, err := requestGeneration("Get", "/products/5", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Contains(t, rr.Body.String(), "Product: 5\n")
+	})
+
+	t.Run("Product key not provided", func(t *testing.T) {
+		rr, err := requestGeneration("Get", "/products", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusNotFound, rr.Code)
+	})
+}
+
+func TestArticlesCategoryHandler(t *testing.T) {
+	t.Run("Valid article category provided", func(t *testing.T) {
+		rr, err := requestGeneration("Get", "/articles/5", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Contains(t, rr.Body.String(), "Category: 5\n")
+	})
+
+	t.Run("Article category not provided", func(t *testing.T) {
+		rr, err := requestGeneration("GET", "/articles", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusNotFound, rr.Code)
+	})
+}
+
+func TestArticleHandler(t *testing.T) {
+	t.Run("Valid article for category provided", func(t *testing.T) {
+		rr, err := requestGeneration("GET", "/articles/5/2", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Contains(t, rr.Body.String(), "Category: 5 - Article: 2\n")
+	})
+
+	t.Run("No article for category provided", func(t *testing.T) {
+		rr, err := requestGeneration("GET", "/articles/5/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusNotFound, rr.Code)
+	})
+
+	t.Run("Invalid article for category provided", func(t *testing.T) {
+		rr, err := requestGeneration("GET", "/articles/5/a", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusNotFound, rr.Code)
+	})
+}
